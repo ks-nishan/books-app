a8acfb1a57004551b8f326280fbdb0f139e6ff96
nishanthan <nishanthan@codevus.com>
2024-06-11 12:03:45 +0530

    feature: Base title added to the top of the page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Nishanthan.K</p>
+    <main className="md:max-3w-xl sm:max-w-2xl mx-auto mt-4">
+      <div className="text-center my-5 flex flex-col gap-4">
+        <h2 className="text-2xl font-bold">Books Application</h2>
+      </div>
     </main>
   );
 }
