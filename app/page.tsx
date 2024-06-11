ca4083076572f1ec7696d00b13b66dd26be6f858
nishanthan <nishanthan@codevus.com>
2024-06-11 12:56:06 +0530

    bug: Max width for the main page fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import AllBooks from "./components/AllBooks";
 
 export default function Home() {
   return (
-    <main className="md:max-3w-xl sm:max-w-2xl mx-auto mt-4">
+    <main className="md:max-w-3xl sm:max-w-2xl mx-auto mt-4">
       <div className="text-center my-5 flex flex-col gap-4">
         <h2 className="text-2xl font-bold">Books Application</h2>
         <AddBook />
