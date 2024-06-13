d0bd08477cdc25ebab9f0a0a2682dc6d72254cea
nishanthan <nishanthan@codevus.com>
2024-06-13 14:19:16 +0530

    refactor: Error handled while adding new book

--- api.ts
+++ api.ts
@@ -1,38 +1,43 @@
 import { Book } from "./types/book";
 
-const baseURL = 'http://localhost:3002';
+const baseURL = "http://localhost:3002";
 
 // function to get all the books from the server
 export const getAllBooks = async (): Promise<Book[]> => {
-    try {
-        const response = await fetch(`${baseURL}/books`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            cache: 'no-store' // To avoid caching issues
-        });
-        
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+  try {
+    const response = await fetch(`${baseURL}/books`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-store", // To avoid caching issues
+    });
 
-        const books: Book[] = await response.json();
-        return books;
-    } catch (error) {
-        throw error;
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+
+    const books: Book[] = await response.json();
+    return books;
+  } catch (error) {
+    throw error;
+  }
 };
 
+// function to add new book to the list
 export const addBook = async (book: Book): Promise<Book> => {
+  try {
     const res = await fetch(`${baseURL}/books`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        cache: 'no-store',
-        body: JSON.stringify(book)
-    })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-store",
+      body: JSON.stringify(book),
+    });
     const newBook = await res.json();
     return newBook;
-}
+  } catch (error) {
+    throw error;
+  }
+};
